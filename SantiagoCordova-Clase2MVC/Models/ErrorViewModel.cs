8cf08d12c603fefe27f912db769275564f87632e
Santiago Córdova <scordova@usfq.edu.ec>
2024-09-25 07:45:40 -0500

    Agregar archivos de proyecto.

---
+++ SantiagoCordova-Clase2MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SantiagoCordova_Clase2MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
